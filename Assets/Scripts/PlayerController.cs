7073d714a308831520c77f7d0b2a145ee322c84a
Edison Li <72610128+Excailibur@users.noreply.github.com>
2025-04-15 17:55:50 -0400

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -163,4 +163,13 @@ public class PlayerController : MonoBehaviour, ISomething, ISomethingB
     {
         lockAnimation = false;
     }
+
+    /**
+     * Some wicked cool lines of code
+     * that does something totally awesome, that WE WANT TO KEEP
+     * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+     * ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+     * ullamco laboris nisi ut aliquip exea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+     * cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+     */
 }
